9f6a2fe37367900fece24d3bcc7fdc346bedb168
CarlosAlternova <carlos.macias@alternova.com>
2024-08-08 23:12:45 -0500

    blog-entry

--- templates/blog.php
+++ templates/blog.php
@@ -8,36 +8,73 @@
   wp_enqueue_style('blog');
 
   $current_page = isset($_GET['current_page']) ? $_GET['current_page'] : 1;
+
+
+  function excerpt($limit) {
+    $excerpt = explode(' ', get_the_excerpt(), $limit);
+    if (count($excerpt)>=$limit) {
+    array_pop($excerpt);
+    $excerpt = implode(" ",$excerpt).'...';
+    } else {
+    $excerpt = implode(" ",$excerpt);
+    }
+    $excerpt = preg_replace('`[[^]]*]`','',$excerpt);
+    return $excerpt;
+  }
 ?>
 
+
+
+
 <?php get_header(); ?>
-<?php
+
+
+<section id="banner-conocenos">
+    <div class="content-banner-transversal">
+        <div class="titulo-banner-transversal">
+            <h1 class="">Blog</h1>
+        </div>
+        <div class="img-banner-transversal" style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
+
+        </div>
+
+    </div>
+
+</section>
+
+<section id="destacados-form">
+    <div class="container">
+        <div class="content-destacados-form">
+            <div class="content-destacados">
+
+
+
+                <?php
                                 $args = array(
                                 'post_type' => 'post',
                                 'post_status' => 'publish',
                                 'posts_per_page' => 2,
-                                'paged' => $current_page
+                               'tax_query' => array( array ( 'taxonomy' => 'category', 'field' => 'slug', 'terms' => 'destacados') )
                                 
                                     );
                     
                                 $entradas = new WP_Query($args);
                                     ?>
 
-<?php if ($entradas->have_posts()) : ?>
+                <?php if ($entradas->have_posts()) : ?>
 
-<?php while ($entradas->have_posts()) : $entradas->the_post();
-
-$total_pages = $entradas->max_num_pages;
+                <?php while ($entradas->have_posts()) : $entradas->the_post();
 
 ?>
 
-<div class="contenedor-entrada-insight" style="margin-top: 5%">
-    <div class="content-post-insight" style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
-        <div class="content-text-insight">
-            <h3><?php the_title(); ?></h3>
-            <div class="content-down-categoria">
-                <ul class="control-categoria">
-                    <?php
+                <a href="<?php echo get_the_permalink(); ?>" class="contenedor-entrada-blog">
+                    <div class="content-img-post-blog"
+                        style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
+
+                    </div>
+                    <div class="content-text-blog">
+                        <ul class="control-categoria">
+                            <?php
                             $categories = get_the_category();
                             if (!empty($categories)) {
                                 foreach ($categories as $category) {
@@ -45,34 +82,128 @@ $total_pages = $entradas->max_num_pages;
                                 }
                             }
                             ?>
-                </ul>
+                        </ul>
+                        <h3><?php the_title(); ?></h3>
+                        <div class="content-calendar-date">
+                            <img src="<?php echo get_site_url(); ?>/wp-content/uploads/2024/08/calendar-post.svg">
+                            <span><?php echo get_the_time('j F Y'); ?></span>
+                        </div>
+                        <p><?php echo excerpt('19'); ?></p>
 
+                    </div>
+                </a>
+                <?php endwhile ?>
+                <?php wp_reset_postdata(); ?>
+                <?php endif ?>
 
             </div>
-            <div class="btn-post">
-                <a href="<?php echo get_the_permalink(); ?>"><img
-                        src="<?php echo get_site_url(); ?>/wp-content/uploads/2024/06/row.png" alt="row-btn"></a>
-            </div>
+            <div class="content-form">
+                <div class="control-form-sidebar">
+                    <h4>Navega por temas de tu interés</h4>
+                    <?php
+// Obtener todas las categorías
+$categories = get_categories();
 
+if ( !empty($categories) ) {
+    echo '<ul class="post-categories">';
+    foreach ( $categories as $category ) {
+        // Mostrar el nombre de la categoría con un enlace a la página de la categoría
+        echo '<li>';
+        echo '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '">';
+        echo esc_html( $category->name );
+        echo '</a>';
+        echo '</li>';
+    }
+    echo '</ul>';
+} else {
+    echo 'No hay categorías disponibles.';
+}
+?>
+
+                    <div class="content-redes-sidebar">
+                        <?php 
+
+get_template_part('/atoms/a-btn-info/a-btn-info', null, 
+    array(
+        'custom_class' => '',
+        'target' => true
+    )
+);
+?>
+                    </div>
+
+                </div>
+            </div>
         </div>
     </div>
-</div>
-<?php endwhile ?>
-<div class="pagination">
-    <?php echo paginate_links(array(
+</section>
+
+<section id="blog-secondsection">
+    <div class="container">
+        <div class="content-posts-second">
+
+
+            <?php
+                                $args = array(
+                                'post_type' => 'post',
+                                'post_status' => 'publish',
+                                'posts_per_page' => 6,
+                                'paged' => $current_page
+                                
+                                    );
+                    
+                                $entradas = new WP_Query($args);
+                                    ?>
+
+            <?php if ($entradas->have_posts()) : ?>
+
+            <?php while ($entradas->have_posts()) : $entradas->the_post();
+
+$total_pages = $entradas->max_num_pages;
+
+?>
+
+            <a href="<?php echo get_the_permalink(); ?>" class="contenedor-entrada-blog second">
+                <div class="content-img-post-blog second"
+                    style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
+
+                </div>
+                <div class="content-text-blog second">
+                    <ul class="control-categoria">
+                        <?php
+                            $categories = get_the_category();
+                            if (!empty($categories)) {
+                                foreach ($categories as $category) {
+                                    echo '<li>' . esc_html($category->name) . '</li>';
+                                }
+                            }
+                            ?>
+                    </ul>
+                    <h3><?php the_title(); ?></h3>
+                    <div class="content-calendar-date">
+                        <img src="<?php echo get_site_url(); ?>/wp-content/uploads/2024/08/calendar-post.svg">
+                        <span><?php echo get_the_time('j F Y'); ?></span>
+                    </div>
+                    <p><?php echo excerpt('19'); ?></p>
+
+                </div>
+            </a>
+            <?php endwhile ?>
+
+            <?php wp_reset_postdata(); ?>
+            <?php endif ?>
+
+        </div>
+        <div class="pagination">
+            <?php echo paginate_links(array(
         'total' => $total_pages,
-        'next_text' => 'Adelante ->',
-        'prev_text' => '<',
+        'next_text' => 'Adelante -->',
+        'prev_text' => '<-- Atrás',
         'format' => '?current_page=%#%',
         'current' => $current_page
     )); ?>
-</div>
-<?php wp_reset_postdata(); ?>
-<?php endif ?>
-
-
-
-
-
+        </div>
+    </div>
+</section>
 
 <?php get_footer(); ?>
