64e1cdac5301bdf4bb079107527d72548c784c38
CarlosAlternova <carlos.macias@alternova.com>
2024-08-13 12:51:13 -0500

    PREGUNTAS-H

--- templates/home/home.js
+++ templates/home/home.js
@@ -1,19 +1,44 @@
 const swiperProjects = () => {
-  const swiperProjects = new Swiper('.swiper-project', {
+  const swiperProjects = new Swiper(".swiper-project", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
     navigation: {
-      nextEl: '.swiper-button-next--project',
-      prevEl: '.swiper-button-prev--project',
+      nextEl: ".swiper-button-next--project",
+      prevEl: ".swiper-button-prev--project",
     },
     pagination: {
-      el: '.swiper-pagination--content-project',
+      el: ".swiper-pagination--content-project",
       clickable: true,
-    }
+    },
   });
-}
-    
-window.addEventListener('DOMContentLoaded', ()=>{
+};
+
+window.addEventListener("DOMContentLoaded", () => {
   swiperProjects();
-})
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const accordionHeaders = document.querySelectorAll(".accordion-header");
+
+  accordionHeaders.forEach((header) => {
+    header.addEventListener("click", function () {
+      const activeHeader = document.querySelector(".accordion-header.active");
+      if (activeHeader && activeHeader !== header) {
+        activeHeader.classList.remove("active");
+        activeHeader.nextElementSibling.style.maxHeight = 0;
+        activeHeader.nextElementSibling.style.padding = "0 15px";
+      }
+
+      header.classList.toggle("active");
+      const accordionContent = header.nextElementSibling;
+      if (header.classList.contains("active")) {
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+        accordionContent.style.padding = "15px";
+      } else {
+        accordionContent.style.maxHeight = 0;
+        accordionContent.style.padding = "0 15px";
+      }
+    });
+  });
+});
