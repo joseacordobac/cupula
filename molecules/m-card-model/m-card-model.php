e21ccc9137edee17b0f22ff4e228d91dc19c2815
joseacordobac <joseacordobac@gmail.com>
2024-09-06 06:15:49 -0500

    information saler place

--- molecules/m-card-model/m-card-model.php
+++ molecules/m-card-model/m-card-model.php
@@ -18,14 +18,39 @@
       <div class="m-card-model__body-text">
         <h3 class="m-card-model__body-city"><?php the_sub_field('city'); ?></h3>
         <h2 class="m-card-model__body-title"><?php get_sub_field('sales_sale_name'); ?></h2>
-        <p class="m-card-model__body-data">
-          <img class="m-card-model__icon" src="<?php echo get_stylesheet_directory_uri() . '/assets/icons/call.svg'; ?>" />
-          <?php the_sub_field('adress'); ?>
-        </p>
-        <p class="m-card-model__body-data">
-          <img class="m-card-model__icon" src="<?php echo get_stylesheet_directory_uri() . '/assets/icons/phone.svg'; ?>" />
-          <?php the_sub_field('phone'); ?>
-        </p>
+
+        <?php if(get_sub_field('phone')): ?>
+          <p class="m-card-model__body-data">
+            <img class="m-card-model__icon" src="<?php echo get_stylesheet_directory_uri() . '/assets/icons/call.svg'; ?>" />
+            <?php the_sub_field('phone'); ?>
+          </p>
+        <?php endif; ?>
+
+        <?php 
+        if(get_sub_field('whatsapp')):
+          $whast_num = get_sub_field('whatsapp'); 
+          $parse = str_replace(['+', ' '], '', $whast_num);
+        ?>
+          <a href="https://wa.me/<?php echo $parse; ?>" class="m-card-model__body-data" target="_blank">
+            <img class="m-card-model__icon" src="<?php echo get_stylesheet_directory_uri() . '/assets/icons/whatsapp.svg'; ?>" />
+            <?php echo $whast_num; ?>
+          </a>
+        <?php endif; ?>
+
+        <?php if(get_sub_field('adress')): ?>
+            <p class="m-card-model__body-data">
+              <img class="m-card-model__icon" src="<?php echo get_stylesheet_directory_uri() . '/assets/icons/phone.svg'; ?>" />
+              <?php the_sub_field('adress') ?>
+            </p>
+        <?php endif; ?>
+
+        <?php if(get_sub_field('horarios_de_atencion')): ?>
+          <p class="m-card-model__body-data">
+            <img class="m-card-model__icon" src="<?php echo get_stylesheet_directory_uri() . '/assets/icons/scheduler.png'; ?>" />
+            <?php the_sub_field('horarios_de_atencion'); ?>
+          </p>
+        <?php endif; ?>
+
       </div>
       <?php 
       template_part_atomic('atoms/a-btn-places/a-btn-places', 
